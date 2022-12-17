1e192447d4b68617ccf3e2d2f7088613c2b495a2
Tom Stefanou <tomstefanou@yahoo.com>
2022-12-17 01:14:52 +0000

    Update SimpleSpy.lua

--- SimpleSpy.lua
+++ SimpleSpy.lua
@@ -521,7 +521,7 @@ end
 local function restoreExcludedRemotes()
 	for _, remoteListType in pairs(remotesListTypes) do
 		if not isfolder(PlaceId) then return end
-		local path = PlaceId .. "/".. type .. ".txt"
+		local path = PlaceId .. "/".. remoteListType .. ".txt"
 		if isfile(path) then
 			print("path is", path)
 			if file then
